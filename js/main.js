0149a7b7dd4f152a6f7331e452e81c229bd596bb
Peter Tribe <peter.tribe@gmail.com>
2022-05-13 16:49:05 -0700

    Update: file read

--- js/main.js
+++ js/main.js
@@ -2,4 +2,11 @@
 
     $(".compat-container").html("Hello <b>world</b>!");
 
+    var titlesJsonPath = '/data/titles.json';
+    $.getJSON(titlesJsonPath, function( data ) {
+        $.each( data, function( key, val ) {
+            console.log(val['name']);
+        });
+    });
+
 });
