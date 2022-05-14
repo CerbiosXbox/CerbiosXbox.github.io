20bc9f3ae3ff0e052f5407235f696b86e04be0f6
Peter Tribe <peter.tribe@gmail.com>
2022-05-13 17:10:09 -0700

    Update: title

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 ﻿$( document ).ready(function() {
 
-    $(".compat-container").html("Hello <b>world</b>!");
-
     var titlesJsonPath = '/data/titles.json';
     $.getJSON(titlesJsonPath, function( data ) {
         $.each( data, function( key, val ) {
