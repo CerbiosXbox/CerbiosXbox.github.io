b2c324046dbdbc6d80e1ff7666d65075bb27bdeb
Peter Tribe <peter.tribe@gmail.com>
2022-05-13 19:53:30 -0700

    Update: display

--- js/main.js
+++ js/main.js
@@ -4,8 +4,17 @@
     $.getJSON(titlesJsonPath, function( data ) {
         $.each( data, function( key, val ) {
             
-            var imageHtml = '<img class="img-fluid lazy" title="' + val['name'] + '" src="/titles/' + val['title_id'] + '_front.jpg" style=""><br/>';
-            $(".compat-container").append(imageHtml);           
+            var html = '<div class="col mb-4 title-card">'
+            html += '<div class="mx-auto title-card-container">'
+            html += '<div class="title-card-image-container">'
+
+            html += '<img class="img-fluid lazy" title="' + val['name'] + '" src="/titles/' + val['title_id'] + '_front.jpg" style=""><br/>';
+         
+            html += '</div>'
+            html += '<div class="fill-playable card-body text-center py-1 my-0"><small><strong>Playable</strong></small></div>'
+            html += '</div>'
+            html += '</div>'
+            $(".results").append(imageHtml);           
 
         });
     });
